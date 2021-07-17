2cb23e3bad8ba994ab7780dbd39ec3a2fdcf4f14
Graff46 <macter46@yandex.ru>
2021-07-18 00:21:17 +0300

    Добавил метод для перехода по локациям из скрипта. Без спавна левел чанжеров

--- src/xrGame/level_script.cpp
+++ src/xrGame/level_script.cpp
@@ -694,7 +694,7 @@ bool ray_pick(const Fvector& start, const Fvector& dir, float range,
 }
 
 // Graff46
-void jump_to_level(const Fvector& m_position, u32 m_level_vertex_id, GameGraph::_GRAPH_ID m_game_vertex_id, const Fvector& m_angles)
+void jump_level(const Fvector& m_position, u32 m_level_vertex_id, GameGraph::_GRAPH_ID m_game_vertex_id, const Fvector& m_angles)
 {
     NET_Packet p;
     p.w_begin(M_CHANGE_LEVEL);
@@ -942,21 +942,8 @@ IC static void CLevel_Export(lua_State* luaState)
         def("translate_string", &translate_string),
         def("reload_language", +[]() { StringTable().ReloadLanguage(); }),
         def("log_stack_trace", &xrDebug::LogStackTrace),
-        def("jump_to_level", +[](pcstr level_name)
-        {
-            if (!ai().game_graph().header().level_exist(level_name))
-            {
-                GEnv.ScriptEngine->script_log(LuaMessageType::Error,
-                    "game.jump_to_level: cannot jump to level '%s' – it doesn't exist", level_name);
-                return;
-            }
-            ai().alife().jump_to_level(level_name);
-        }),
-        def("jump_to_level", &jump_to_level),
-        def("jump_to_level", +[](const Fvector& m_position, u32 m_level_vertex_id, GameGraph::_GRAPH_ID m_game_vertex_id)
-        {
-            jump_to_level(m_position, m_level_vertex_id, m_game_vertex_id, {});
-        })
+        def("translate_string", &translate_string),
+        def("jump_level", &jump_level)
     ];
 };
 
