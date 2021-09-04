31a715cab5ee4820624ae3c4365c3eaa1b61677e
Graff46 <macter46@yandex.ru>
2021-09-05 01:05:46 +0300

    Revert "Доделал перенос"
    This reverts commit 915f5b3d6decb6c6a66de5c0dfc3de177f6b4b3b.

--- src/xrGame/ui/UIActorMenu.h
+++ src/xrGame/ui/UIActorMenu.h
@@ -253,10 +253,6 @@ public:
 
 protected:
     CUIDragDropListEx* GetSlotList(u16 slot_idx);
-	
-	//-> i-love-kfc: Ограничение объема из Unexplored Land
-	CUIDragDropListEx*			m_pDeadBodyBagList;
-	
     bool CanSetItemToList(PIItem item, CUIDragDropListEx* l, u16& ret_slot);
 
     xr_vector<EDDListType> m_allowed_drops[iListTypeMax];
