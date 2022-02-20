63866899cd4273c53f1e05bbe20af57d6ffd5167
Graff46 <macter46@yandex.ru>
2022-02-20 17:26:48 +0300

    Добавил назначение префикса "голоса" НПС в его спавн секции
    В спавн секции НПС можно задать ключ `voice_prefix` это равносильно `snd_config` из XML конфига НПС

--- src/xrGame/ai/stalker/ai_stalker.cpp
+++ src/xrGame/ai/stalker/ai_stalker.cpp
@@ -110,7 +110,9 @@ void CAI_Stalker::reinit()
     //	movement().reinit				();
 
     //загрузка спецевической звуковой схемы для сталкера согласно m_SpecificCharacter
-    sound().sound_prefix(SpecificCharacter().sound_voice_prefix());
+    sound().sound_prefix(
+        pSettings->read_if_exists(*cNameSect(), "voice_prefix", SpecificCharacter().sound_voice_prefix())
+    );
 
     LoadSounds(*cNameSect());
 
